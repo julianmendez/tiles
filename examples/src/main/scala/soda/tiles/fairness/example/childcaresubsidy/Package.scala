f23e6b970002fdb0b9fd4be6d344c41373c985e0
Julian Mendez <julian.mendez@gmail.com>
2024-03-02 09:44:40 +0100

    Add example of single guardian

--- examples/src/main/scala/soda/tiles/fairness/example/childcaresubsidy/Package.scala
+++ examples/src/main/scala/soda/tiles/fairness/example/childcaresubsidy/Package.scala
@@ -16,10 +16,13 @@ import   soda.tiles.fairness.tile.AllAtLeastTile
 import   soda.tiles.fairness.tile.AllEqual1Tile
 import   soda.tiles.fairness.tile.AllEqualTile
 import   soda.tiles.fairness.tile.AllSatisfyPTile
+import   soda.tiles.fairness.tile.AtomicZipTile
 import   soda.tiles.fairness.tile.AttributePTile
+import   soda.tiles.fairness.tile.CombineBooleanTile
 import   soda.tiles.fairness.tile.CorrelationTile
 import   soda.tiles.fairness.tile.DecisionTile
 import   soda.tiles.fairness.tile.FalsePosTile
+import   soda.tiles.fairness.tile.FilterActorTile
 import   soda.tiles.fairness.tile.MapPTile
 import   soda.tiles.fairness.tile.NeededPTile
 import   soda.tiles.fairness.tile.PredictionPTile
@@ -102,5 +105,88 @@ case class CcsPerFamilyPipeline_ (sigma : Measure => Measure => Measure, p_utili
 object CcsPerFamilyPipeline {
   def mk (sigma : Measure => Measure => Measure) (p_utility : Resource => Measure) : CcsPerFamilyPipeline =
     CcsPerFamilyPipeline_ (sigma, p_utility)
+}
+
+
+trait CcsSingleGuardianPipeline
+{
+
+  def   sigma : Measure => Measure => Measure
+  def   p_utility : Resource => Measure
+  def   adults : Actor => Measure
+
+  def is_equals_0 (measure : Measure) : Boolean =
+    measure match  {
+      case Some (0) => true
+      case otherwise => false
+    }
+
+  lazy val all_satisfy_p_tile = AllSatisfyPTile .mk (is_equals_0)
+
+  lazy val all_equal_tile = AllEqualTile .mk
+
+  lazy val received_sigma_p_tile = ReceivedSigmaPTile .mk (sigma) (p_utility)
+
+  lazy val all_actor_tile = AllActorTile .mk
+
+  lazy val all_actor_pair_tile = AllActorPairTile .mk
+
+  lazy val unzip_fst_tile = UnzipPairFstTile .mk
+
+  lazy val unzip_snd_tile = UnzipPairSndTile .mk
+
+  def and_combination (b0 : Boolean) (b1 : Boolean) : Boolean =
+    b0 && b1
+
+  lazy val and_tile = CombineBooleanTile .mk (and_combination)
+
+  lazy val atomic_zip_tile = AtomicZipTile .mk
+
+  def condition_0 (a : Actor) : Boolean =
+    adults (a) .getOrElse (0) == 1
+
+  def condition_1 (a : Actor) : Boolean =
+    adults (a) .getOrElse (0) > 1
+
+  lazy val filter_actor_tile_0 = FilterActorTile .mk (condition_0)
+
+  lazy val filter_actor_tile_1 = FilterActorTile .mk (condition_1)
+
+  def get_branch_0 (message : TileMessage [Seq [TilePair [Actor, Actor] ] ] )
+      : TileMessage [Boolean] =
+    all_equal_tile .apply (
+      received_sigma_p_tile .apply (
+        filter_actor_tile_0 .apply (
+          unzip_fst_tile .apply (message)
+        )
+      )
+    )
+
+  def get_branch_1 (message : TileMessage [Seq [TilePair [Actor, Actor] ] ] )
+      : TileMessage [Boolean] =
+    all_satisfy_p_tile .apply (
+      received_sigma_p_tile .apply (
+        filter_actor_tile_1 .apply (
+          unzip_snd_tile .apply (message)
+        )
+      )
+    )
+
+  def apply (message : TileMessage [Boolean] ) : TileMessage [Boolean] =
+    and_tile .apply (
+      atomic_zip_tile .apply (
+        get_branch_0 (all_actor_pair_tile .apply (message) )
+      ) (
+        get_branch_1 (all_actor_pair_tile .apply (message) )
+      )
+    )
+
+}
+
+case class CcsSingleGuardianPipeline_ (sigma : Measure => Measure => Measure, p_utility : Resource => Measure, adults : Actor => Measure) extends CcsSingleGuardianPipeline
+
+object CcsSingleGuardianPipeline {
+  def mk (sigma : Measure => Measure => Measure) (p_utility : Resource => Measure) (adults : Actor => Measure) : CcsSingleGuardianPipeline =
+    CcsSingleGuardianPipeline_ (sigma, p_utility, adults)
 }
 
