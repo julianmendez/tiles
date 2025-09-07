ac33387aade1a801cd019c0500614177b51a6be9
Julian Mendez <julian.mendez@gmail.com>
2025-09-07 22:00:03 +0200

    Add SumCountTile

--- core/src/main/scala/soda/tiles/fairness/tile/derived/fold/Package.scala
+++ core/src/main/scala/soda/tiles/fairness/tile/derived/fold/Package.scala
@@ -18,6 +18,40 @@ import   soda.tiles.fairness.tool.TileTriple
 
 
 
+/**
+ * This processes a sequence of measures and returns a pair, which has the sum on the first component and
+ * the number of elements on the second component.
+ */
+
+trait SumCountTile
+{
+
+  lazy val zero : Measure = MeasureMod .mk .zero
+
+  lazy val one : Measure = MeasureMod .mk .one
+
+  lazy val pair_zero : TilePair [Measure, Measure] =
+    TilePair .mk (zero) (zero)
+
+  def combine (acc : TilePair [Measure, Measure] ) (elem : Measure)
+      : TilePair [Measure, Measure] =
+    TilePair .mk (MeasureMod .mk .plus (acc .fst) (elem) ) (MeasureMod .mk .plus (acc .snd) (one) )
+
+  lazy val fold_tile = FoldTile .mk (pair_zero) (combine)
+
+  def apply (message : TileMessage [Seq [Measure] ] ) : TileMessage [TilePair [Measure, Measure] ] =
+    fold_tile .apply (message)
+
+}
+
+case class SumCountTile_ () extends SumCountTile
+
+object SumCountTile {
+  def mk : SumCountTile =
+    SumCountTile_ ()
+}
+
+
 /*
 directive lean
 import Soda.tiles.fairness.tool.TileMessage
@@ -25,7 +59,7 @@ import Soda.tiles.fairness.tool.TileMessage
 
 /**
  * This takes a sequence, a starting value, and a function, then processes the sequence from left to right,
- * combining elements into a single result step by step.
+ * adding elements into a single result step by step.
  */
 
 trait SumPhiTile [A ]
@@ -33,12 +67,12 @@ trait SumPhiTile [A ]
 
   def   phi : A => Measure
 
-  lazy val measure_zero : Measure = Some (0)
+  lazy val zero : Measure = MeasureMod .mk .zero
 
   def combine (acc : Measure) (elem : A) : Measure =
     MeasureMod .mk .plus (acc) (phi (elem) )
 
-  lazy val fold_tile = FoldTile .mk (measure_zero) (combine)
+  lazy val fold_tile = FoldTile .mk (zero) (combine)
 
   def apply (message : TileMessage [Seq [A] ] ) : TileMessage [Measure] =
     fold_tile
--- core/src/test/scala/soda/tiles/fairness/tile/derived/fold/Package.scala
+++ core/src/test/scala/soda/tiles/fairness/tile/derived/fold/Package.scala
@@ -17,6 +17,56 @@ import   soda.tiles.fairness.tool.TilePair
 
 
 
+case class SumCountTileSpec ()
+  extends
+    AnyFunSuite
+{
+
+  def check [A ] (obtained : A) (expected : A) : org.scalatest.compatible.Assertion =
+    assert(obtained == expected)
+
+  lazy val scenario = ScenarioExample .mk
+
+  def mk_tile_message (seq : Seq [Int] ) : TileMessage [Seq [Measure] ] =
+    TileMessageBuilder
+      .mk
+      .build (scenario .context) (scenario .outcome0) (seq .map ( x => Some (x) ) )
+
+  lazy val sum_count_tile = SumCountTile .mk
+
+  test ("sum and count on empty sequence") (
+    check(
+      obtained = sum_count_tile
+        .apply (mk_tile_message (Seq [Int] () ) )
+        .contents
+    ) (
+      expected = TilePair .mk (Some (0) ) (Some (0) )
+    )
+  )
+
+  test ("sum and count on single element sequence") (
+    check (
+      obtained = sum_count_tile
+        .apply (mk_tile_message (Seq [Int] (5) ) )
+        .contents
+    ) (
+      expected = TilePair .mk (Some (5) ) (Some (1) )
+    )
+  )
+
+  test ("sum and count on multiple elements sequence") (
+    check (
+      obtained = sum_count_tile
+        .apply (mk_tile_message (Seq [Int] (1 , 2 , 3 , 4) ) )
+        .contents
+    ) (
+      expected = TilePair .mk (Some(10) ) (Some(4) )
+    )
+  )
+
+}
+
+
 case class SumPhiTileSpec ()
   extends
     AnyFunSuite
