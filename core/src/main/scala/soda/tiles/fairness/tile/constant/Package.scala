40473ae1544295d0540bf0bfc6b9bd9536e9fb2b
Julian Mendez <julian.mendez@gmail.com>
2025-09-09 23:10:10 +0200

    Add AllResourceTile

--- core/src/main/scala/soda/tiles/fairness/tile/constant/Package.scala
+++ core/src/main/scala/soda/tiles/fairness/tile/constant/Package.scala
@@ -5,6 +5,7 @@ package soda.tiles.fairness.tile.constant
  */
 
 import   soda.tiles.fairness.tool.Agent
+import   soda.tiles.fairness.tool.Resource
 import   soda.tiles.fairness.tool.TileMessage
 import   soda.tiles.fairness.tool.TileMessageBuilder
 import   soda.tiles.fairness.tool.TilePair
@@ -46,5 +47,40 @@ case class AllAgentTile_ () extends AllAgentTile
 object AllAgentTile {
   def mk : AllAgentTile =
     AllAgentTile_ ()
+}
+
+
+/*
+directive lean
+import Soda.tiles.fairness.tool.TileMessage
+*/
+
+/**
+ * This tile returns a sorted sequence of resources, where each resource occurs exactly once.
+ */
+
+trait AllResourceTile
+{
+
+
+
+  def all_resources (message : TileMessage [Boolean] ) : Seq [Resource] =
+      ( (message .outcome) .assignments)
+        .map ( assignment => assignment .resource)
+        .distinct
+        .sorted
+
+  def apply (message : TileMessage [Boolean] ) : TileMessage [Seq [Resource] ] =
+    TileMessageBuilder .mk .build (message .context) (message .outcome) (
+      all_resources (message)
+    )
+
+}
+
+case class AllResourceTile_ () extends AllResourceTile
+
+object AllResourceTile {
+  def mk : AllResourceTile =
+    AllResourceTile_ ()
 }
 
